8406d50f43d3e02dd3c55c503a5fef736bf9174f
Tomek Bloch <zenekkrowa@gmail.com>
2020-02-06 16:44:57 +0100

    added like visible blob so thats that and also started implementing basic math to calculate stuff like relativity between score and radius

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-
-function setup(){
+let player
+function setup() {
     createCanvas(innerWidth, innerHeight);
-
+    player = new Blob(
+        0,
+        0,
+        10000,
+        30,
+        200,
+        40
+    );
+    background(0)
+    print(player)
 }
-function draw(){
-    background(0);
-    
+function draw() {
+    background(0)
+    player.update();
+    player.show();
+    stroke(255)
+    strokeWeight(20)
+    point(400, 400)
 }
--- urBlob.js
+++ urBlob.js
@@ -1,6 +1,21 @@
 class Blob {
-    constructor(){
-        
+    constructor(x, y, score, r, g, b) {
+        this.x = x;
+        this.y = y;
+        this.r = sqrt(score / PI);
+        this.red = r;
+        this.green = g;
+        this.blue = b;
+    }
+    update() {
+        translate(width / 2, height / 2);
+
+    }
+    show() {
+        stroke(this.red + 50, this.green + 50, this.blue + 50);
+        strokeWeight(0.1 * this.r);
+        fill(this.red, this.green, this.blue);
+        ellipse(this.x, this.y, this.r * 2);
     }
 
 }
