3fac3e7447376a5480462509413f7f9deeee35fe
Tomek Bloch <zenekkrowa@gmail.com>
2020-02-07 15:28:57 +0100

    chyba nic nie zmieniłem

--- Border.js
+++ Border.js
@@ -15,9 +15,16 @@ class Border {
         let move = createVector(-(mouseX - width / 2), -(mouseY - height / 2)) //same shit as in food
         move.normalize()
         move = p5.Vector.mult(move, 1000 / sqrt(other.score))
-        this.lu.add(move)
-        this.ld.add(move)
-        this.rd.add(move)
-        this.ru.add(move)
+        if (other.pos.x <= this.lu.x || other.pos.x >= this.rd.x) {         //those ifs make sure that 
+            if (other.pos.y <= this.lu.y || other.pos.y >= this.rd.x) {     // cannot cross borders
+  
+            } else {
+                this.lu.add(move)
+                this.ld.add(move)
+            }
+        } else {
+            this.rd.add(move)
+            this.ru.add(move)
+        }
     }
 }
